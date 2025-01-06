36f54c1967fd00e6e03613d2930f06c82b173326
Bryan <bryan.prolong@outlook.fr>
2025-01-06 21:29:13 +0100

    chore: add tailwind and primevue

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -1,11 +1,14 @@
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/inertia.ts" />
 
-import '../css/app.css'
-import { createSSRApp, h } from 'vue'
-import type { DefineComponent } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { updatePreset } from '@primevue/themes'
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
+import type { DefineComponent } from 'vue'
+import { createSSRApp, h } from 'vue'
+import '../css/app.css'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
@@ -24,6 +27,29 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createSSRApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(PrimeVue, {
+        theme: {
+          preset: Aura,
+        },
+      })
       .mount(el)
+
+    updatePreset({
+      semantic: {
+        primary: {
+          50: '{cyan.50}',
+          100: '{cyan.100}',
+          200: '{cyan.200}',
+          300: '{cyan.300}',
+          400: '{cyan.400}',
+          500: '{cyan.500}',
+          600: '{cyan.600}',
+          700: '{cyan.700}',
+          800: '{cyan.800}',
+          900: '{cyan.900}',
+          950: '{cyan.950}',
+        },
+      },
+    })
   },
 })
