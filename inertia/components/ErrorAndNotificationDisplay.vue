e0061110acf9d2ae4b769bbcad042b627c03d634
Bryan <bryan.prolong@outlook.fr>
2025-02-23 08:14:40 +0100

    refactor: use shadcn for login page

--- inertia/components/ErrorAndNotificationDisplay.vue
+++ inertia/components/ErrorAndNotificationDisplay.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import Message from 'primevue/message'
+import { AlertCircle } from 'lucide-vue-next'
 import { computed } from 'vue'
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert'
 import { usePageErrorsBag } from '~/composables/use_page_errors_bag'
 import { usePageNotification } from '~/composables/use_page_notifications'
 
@@ -16,11 +17,21 @@ const errors = computed(() =>
 
 <template>
   <div v-if="errors.length > 0 || notification">
-    <Message severity="error" v-for="error in errors">
-      {{ error }}
-    </Message>
-    <Message v-if="notification" :severity="notification.type" closable>
-      {{ notification.message }}
-    </Message>
+    <Alert variant="destructive" v-for="error in errors">
+      <AlertCircle class="w-4 h-4" />
+      <AlertTitle>{{ $t('error') }}</AlertTitle>
+      <AlertDescription>{{ error }}</AlertDescription>
+    </Alert>
+
+    <Alert :variant="notification.type === 'error' ? 'destructive' : 'default'" v-if="notification">
+      <AlertCircle class="w-4 h-4" />
+      <AlertTitle>
+        <template v-if="notification.type === 'error'">{{ $t('error') }}</template>
+        <template v-else-if="notification.type === 'success'">{{ $t('success') }}</template>
+        <template v-else-if="notification.type === 'info'">{{ $t('info') }}</template>
+        <template v-else-if="notification.type === 'warn'">{{ $t('warning') }}</template>
+      </AlertTitle>
+      <AlertDescription>{{ notification.message }}</AlertDescription>
+    </Alert>
   </div>
 </template>
--- inertia/components/ui/Link.vue
+++ inertia/components/ui/Link.vue
@@ -7,11 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <InertiaLink
-    class="text-primary cursor-pointer rounded-sm outline-primary outline-offset-1 outline-1 hover:text-primary-emphasis active:text-primary-emphasis-alt focus-visible:outline"
-    :href="href"
-    v-bind="$attrs"
-  >
+  <InertiaLink class="underline" :href="href" v-bind="$attrs">
     <slot />
   </InertiaLink>
 </template>
