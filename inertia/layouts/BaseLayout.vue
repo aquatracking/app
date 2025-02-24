1196e759296ce7d2987dcdb7be261131d02fc5f3
Bryan <bryan.prolong@outlook.fr>
2025-02-24 20:20:41 +0100

    refactor: delete primevue

--- inertia/layouts/BaseLayout.vue
+++ inertia/layouts/BaseLayout.vue
@@ -1,29 +1,19 @@
 <script setup lang="ts">
 import { Head, usePage } from '@inertiajs/vue3'
-import { PrimeVueLocaleOptions, usePrimeVue } from 'primevue/config'
-import { useI18n } from 'vue-i18n'
-import ConfirmDialog from 'primevue/confirmdialog'
 import { useColorMode } from '@vueuse/core'
+import { useI18n } from 'vue-i18n'
 
 defineProps<{ title: string }>()
 
-const { locale, messages } = useI18n()
+const { locale } = useI18n()
 const page = usePage()
 
 locale.value = page.props.locale as string
 
 useColorMode()
-
-const primevue = usePrimeVue()
-
-primevue.config.locale = messages.value[locale.value as string]?.primevue as
-  | PrimeVueLocaleOptions
-  | undefined
 </script>
 <template>
   <Head :title="title" />
-
-  <ConfirmDialog> </ConfirmDialog>
 
   <slot />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -53,5 +53,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-primeui'), require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate')],
 }
